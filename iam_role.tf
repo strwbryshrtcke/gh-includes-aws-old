c8b006294c0beed0b28d6685aa0f3b88f5ad9412
Kanumuru Rajesh <rajesh@we45.com>
2023-06-20 08:13:33 +0530

    removed s3 backend to dynamic

--- iam_role.tf
+++ iam_role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "customer_role" {
-  name               = "customer_role"
+  name               = "customer_role_terraform"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
