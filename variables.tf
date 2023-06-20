075dcea0bd552d8a74e980fcb74227d290fbdb03
Kanumuru Rajesh <rajesh@we45.com>
2023-06-20 07:15:42 +0530

    added terraform files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr1" {
+  default = "10.0.0.0/24"
+}
+
+variable "public_subnet_cidr2" {
+  default = "10.0.1.0/24"
+}
